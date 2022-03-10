6fe269f1f228dd24ebb584f239ca6404250a9002
Oluebube5 <oluebubechukwugloria@gmail.com>
2022-03-10 15:09:21 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ * main - prints the phrase
+ * Return: 0 if existed properly, non-zero if not.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ *  main - prints a phrase
+ *  Return: 0 if exited properly, and non-zero if not
+ */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
