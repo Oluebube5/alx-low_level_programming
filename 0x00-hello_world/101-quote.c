228e3c66c16365ad9fe3cf98fac5b1207572b24b
Oluebube5 <oluebubechukwugloria@gmail.com>
2022-03-10 15:19:31 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdio.h>
 
+/**
+ * main - prints a phrase
+ * followed by a new line, to standard error.
+ * Return: always 0
+ */
 int main(void)
 {
 	write(2, "and that piece ofart is useful\" - Dora Korper, 2015-10-19\n", 59);
