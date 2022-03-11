f8fd59b8779851f1d3dfb7d29c46335038117171
Oluebube5 <oluebubechukwugloria@gmail.com>
2022-03-11 00:17:07 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  * main - prints the phrase
  * Return: 0 if existed properly, non-zero if not.
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
