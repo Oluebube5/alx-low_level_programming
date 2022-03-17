4afc8e20f117ba458f6f2cbc43717298dad668e7
Oluebube5 <oluebubechukwugloria@gmail.com>
2022-03-17 02:05:51 -0700

    commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,7 @@
-#ifindf MAIN_H
-$define MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#inlude <stdio.h>
+#include <stdio.h>
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
